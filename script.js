468a105efad1f9f88a23b60c7fcb834c1d7e1478
CatHub <165255694+CatHub027@users.noreply.github.com>
2024-06-03 09:01:04 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('run-button').addEventListener('click', function() {
+  var code = document.getElementById('code-editor').value;
+  try {
+    var result = eval(code);
+    document.getElementById('output').innerHTML = '<pre>' + result + '</pre>';
+  } catch (error) {
+    document.getElementById('output').innerHTML = '<pre style="color: red;">' + error.message + '</pre>';
+  }
+});
+
+document.getElementById('clear-button').addEventListener('click', function() {
+  document.getElementById('code-editor').value = '';
+  document.getElementById('output').innerHTML = '';
+});
