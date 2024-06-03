1ad63616569932491f12bb4bd6696949a171d224
CatHub <165255694+CatHub027@users.noreply.github.com>
2024-06-03 09:33:31 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("executeBtn").addEventListener("click", function() {
-  var code = document.getElementById("codeInput").value;
-  try {
-    var result = eval(code);
-    document.getElementById("console").innerText = "Output: " + result;
-  } catch (error) {
-    document.getElementById("console").innerText = "Error: " + error.message;
-  }
-});
