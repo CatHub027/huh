2644560931a9144947942ceade7719c1e9855cf6
CatHub <165255694+CatHub027@users.noreply.github.com>
2024-06-03 09:09:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById('run-button').addEventListener('click', function() {
   var code = document.getElementById('code-editor').value;
   try {
-    var result = eval(code);
+    var result = lua.eval(code);
     document.getElementById('output').innerHTML = '<pre>' + result + '</pre>';
   } catch (error) {
     document.getElementById('output').innerHTML = '<pre style="color: red;">' + error.message + '</pre>';
